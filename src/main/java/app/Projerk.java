934991b2962f2be6635ccc4ed997154ffa9fcc68
duong <ngducduong305@gmail.com>
2024-09-06 10:12:48 +0700

    update structure

---
+++ src/main/java/app/Projerk.java
@@ -0,0 +1,32 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Projerk extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Thay đổi đường dẫn đến FXML
+            String fxmlPath = Paths.get("src", "main", "resources", "view", "MainView.fxml").toAbsolutePath().toString();
+            FXMLLoader loader = new FXMLLoader(Paths.get(fxmlPath).toUri().toURL());
+            Parent root = loader.load();
+
+            primaryStage.setTitle("Projerk");
+            primaryStage.setScene(new Scene(root, 400, 300));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error loading FXML: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.Person;
+
+public class MainController {
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    private void handleSubmit() {
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+
+        Person person = new Person(firstName, lastName);
+        resultLabel.setText("Hello, " + person.getFirstName() + " " + person.getLastName() + "!");
+    }
+}
