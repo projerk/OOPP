72f4f2881275ccabb942b955a32f7529fadc66f2
duon( <ngducduong305@gmail.com>
2024-09-29 13:06:49 +0700

    add image to popup

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -6,6 +6,7 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -17,6 +18,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import app.Projerk;
+import io.opentelemetry.sdk.trace.samplers.SamplingResult;
 
 public class DashboardController {
 
@@ -77,27 +79,59 @@ public class DashboardController {
         exitButton.setOnAction(event -> {
             flyOut(temp);
         });
-
-        temp.getChildren().add(exitButton);
         
         VBox bookDetailContainer = new VBox();
-        // VBox.setVgrow(bookDetailContainer, Priority.ALWAYS);
         HBox.setHgrow(bookDetailContainer, Priority.ALWAYS);
         bookDetailContainer.setStyle("-fx-background-color: #ce82ff; -fx-background-radius: 10px;");
 
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setFitToWidth(true);
 
-        ImageView imageView = new ImageView();
-        Image image = new Image("https://bookworm.vnu.edu.vn/ViewEImage.ashx?id=181210");
-        imageView.setImage(image);
-        imageView.setPreserveRatio(true);
-        imageView.setFitWidth(300);
-
-        bookDetailContainer.getChildren().add(imageView);
+        // ImageView imageView = new ImageView();
+        // Image image = new Image("https://bookworm.vnu.edu.vn/ViewEImage.ashx?id=181210");
+        // imageView.setImage(image);
+        // imageView.setPreserveRatio(true);
+        // imageView.setFitWidth(200);
+        ImageView bookImage = createImage("https://bookworm.vnu.edu.vn/ViewEImage.ashx?id=181210", 200);
+        VBox bookInformationContainer = createBookInformation("Sapiens", "sex", "duon9", "Projerk", 1510);
+        HBox information = new HBox();
+        bookDetailContainer.setStyle("-fx-padding: 10 10 10 10");
+        information.getChildren().add(bookImage);
+        information.getChildren().add(bookInformationContainer);
+        bookDetailContainer.getChildren().add(information);
         scrollPane.setContent(bookDetailContainer);
 
+
+        temp.getChildren().add(exitButton);
         temp.getChildren().add(scrollPane);
+        return temp;
+    }
+
+    private ImageView createImage(String url, double size) {
+        ImageView imageView = new ImageView();
+        Image image = new Image(url);
+        imageView.setImage(image);
+        imageView.setPreserveRatio(true);
+        imageView.setFitWidth(200);
+
+        return imageView;
+    }
+
+    private VBox createBookInformation(String bookName, String bookType, String bookAuthors, String bookPublishingHouses, int bookPublishingYear) {
+        VBox temp = new VBox();
+        HBox.setHgrow(temp, Priority.ALWAYS);
+        Label bookNameLabel = new Label("Name: ");
+        Label bookTypeLabel = new Label("Type: ");
+        Label bookAuthorLabel = new Label("Authors: ");
+        Label bookPublishingHouseLabel = new Label("Publishing House: ");
+        Label bookPublishingYearLabel = new Label("Publishing Year: ");
+
+        temp.getChildren().add(bookNameLabel);
+        temp.getChildren().add(bookTypeLabel); 
+        temp.getChildren().add(bookAuthorLabel); 
+        temp.getChildren().add(bookPublishingHouseLabel); 
+        temp.getChildren().add(bookPublishingYearLabel);    
+
         return temp;
     }
 }
